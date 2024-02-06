98b9de7ed9535b0302797f9820accb3fd6b24a24
devsolux <devsolux@gmail.com>
2024-02-06 17:22:14 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Laravel UI for AdminLTE
+
+[Laravel Frontend Scaffolding](https://laravel.com/docs/10.x/frontend) for [AdminLTE3](https://adminlte.io/themes/v3/) Theme.
+
+## License
+
+The MIT License (MIT). Please see [License File](LICENSE) for more information.
+
+
---
+++ resources/views/components/adminlte-layout.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>{{ config('app.name') }}</title>
+
+    <!-- Tell the browser to be responsive to screen width -->
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-1ycn6IcaQQ40/MKBW2W4Rhis/DbILU74C1vSrLJxCq57o941Ym01SwNsOMqvEBFlcgUa6xLiPY/NS5R+E6ztJQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    @vite('resources/sass/app.scss')
+    @stack('third_party_stylesheets')
+    @stack('page_css')
+
+</head>
+
+{{ $slot }}
+
+@vite('resources/js/app.js')
+@stack('third_party_scripts')
+@stack('page_scripts')
+
+</html>
